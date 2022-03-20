0ea203d04d3f4eed5e2e232ec0d05d94fc96c56c
NabidAkash1221~ <nabid15-13826@diu.edu.bd>
2022-03-20 23:30:55 +0600

    Completed task 2:  Added Course Model in the DB Context and Generated all basic API's for this model.

---
+++ DemoAPI/Controllers/CourseController.cs
@@ -0,0 +1,122 @@
+﻿using DemoAPI.DbContexts;
+using DemoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly StudentDbContext _context;
+
+        public CourseController(StudentDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Course
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Course>>> GetCourse()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+
+        // GET: api/Course/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Course>> GetCourse(int id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return course;
+        }
+
+        // PUT: api/Students/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCourse(int id, Course Courses)
+        {
+            if (id != Courses.Id)
+            {
+                return BadRequest();
+            }
+
+            if (Courses is null)
+            {
+                throw new ArgumentNullException(nameof(Courses));
+            }
+
+            _context.Entry(Courses).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CourseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        // POST: api/Students
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Student>> Post(Course Courses)
+        {
+            _context.Courses.Add(Courses);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (CourseExists(Courses.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetCourse", new { id = Courses.Id }, Courses);
+        }
+
+        // DELETE: api/Course/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCourse(int id)
+        {
+            var Course = await _context.Courses.FindAsync(id);
+            if (Course == null)
+            {
+                return NotFound();
+            }
+
+            _context.Courses.Remove(Course);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CourseExists(int id)
+        {
+            return _context.Courses.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ DemoAPI/Models/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoAPI.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+        public string CourseTeacher { get; set; }
+        public int Credit { get; set; }
+    }
+}
